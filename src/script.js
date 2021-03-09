93a9b35508773ccca9b8d167b603f5c0d40489b4
Viacheslav Pliusnin <via.plsn@gmail.com>
2021-03-09 17:10:40 +0200

    functions getSubjects, getAverageMark, getStudentInfo added.

--- src/script.js
+++ src/script.js
@@ -1,1 +1,61 @@
-console.log('Hello, World!')
+const students = [{
+    name: "Tanya",
+    course: 3,
+    subjects: {
+    math: [4, 4, 3, 4],
+    algorithms: [3, 3, 3, 4, 4, 4],
+    data_science: [5, 5, 3, 4]
+    }
+}, {
+    name: "Victor",
+    course: 4,
+    subjects: {
+    physics: [5, 5, 5, 3],
+    economics: [2, 3, 3, 3, 3, 5],
+    geometry: [5, 5, 2, 3, 5]
+    }
+}, {
+    name: "Anton",
+    course: 2,
+    subjects: {
+    statistics: [4, 5, 5, 5, 5, 3, 4, 3, 4, 5],
+    english: [5, 3],
+    cosmology: [5, 5, 5, 5]
+    }
+}];
+
+function callbackSubjects(subject) {
+    return (subject[0].toUpperCase() + subject.slice(1).toLowerCase()).split('_').join(' ');
+}
+function getSubjects(student = students[0]) {
+    const studentValues = Object.values(student);
+    const studentSubjects = Object.keys(studentValues[2]);
+    const newArrayOfSubjects = studentSubjects.map(callbackSubjects);
+    return newArrayOfSubjects;
+}
+console.log(`Student ${students[0].name} subjects:`, getSubjects());
+
+function callbackMarks(accumulator, currentNumber) {
+    return accumulator + currentNumber;
+}
+function getAverageMark(student = students[0]) {
+    const studentValues = Object.values(student);
+    const studentMarks = Object.values(studentValues[2]);
+    const arrayEmptyMarks = [];
+    const arrayMarks = arrayEmptyMarks.concat(...studentMarks);
+    return +(arrayMarks.reduce(callbackMarks, 0) / arrayMarks.length).toFixed(2);
+}
+console.log(`Student ${students[0].name} average mark:`, getAverageMark());
+
+function getStudentInfo(student = students[0]) {
+    const arrayStudent = Object.entries(student);
+    const studentArray = arrayStudent[0].concat(arrayStudent[1]);
+    const studentName = studentArray[1];
+    const studentCourse = studentArray[3];
+    return student = {
+        course: studentCourse,
+        name: studentName,
+        averageMark: getAverageMark(student)
+    }
+}
+console.log(`Student ${students[0].name} info:`, getStudentInfo());
