bc0c9510f6ed5e457cb339cfc0f773bc72f0d7e2
Viacheslav Pliusnin <via.plsn@gmail.com>
2021-03-10 15:49:49 +0200

    functions getStudentsNames, getBestStudent, calculateWordLetters added.

--- src/script.js
+++ src/script.js
@@ -24,6 +24,7 @@ const students = [{
     }
 }];
 
+//Створіть функцію яка повертає список предметів для конкретного студента. Зверніть увагу – назву предмету необхідно повертати з великої літери, а "_" – замінити на пробіл.
 function callbackSubjects(subject) {
     return (subject[0].toUpperCase() + subject.slice(1).toLowerCase()).split('_').join(' ');
 }
@@ -35,6 +36,7 @@ function getSubjects(student = students[0]) {
 }
 console.log(`Student ${students[0].name} subjects:`, getSubjects());
 
+//Створіть функцію яка поверне середню оцінку по усім предметам для переданого студента. Оцінку округліть до 2-го знаку після коми.
 function callbackMarks(accumulator, currentNumber) {
     return accumulator + currentNumber;
 }
@@ -47,6 +49,7 @@ function getAverageMark(student = students[0]) {
 }
 console.log(`Student ${students[0].name} average mark:`, getAverageMark());
 
+//Створіть функцію яка повертає інформацію загального виду по переданому студенту. Повинна бути виведена інформація: курс, ім'я, середня оцінка.
 function getStudentInfo(student = students[0]) {
     const arrayStudent = Object.entries(student);
     const studentArray = arrayStudent[0].concat(arrayStudent[1]);
@@ -59,3 +62,38 @@ function getStudentInfo(student = students[0]) {
     }
 }
 console.log(`Student ${students[0].name} info:`, getStudentInfo());
+
+//Ствроіть функцію яка повертає імена студентів у алфавітному порядку.
+function getStudentsNames(studentsArray = students) {
+    let namesArray = [];
+    for (let i = 0; i < studentsArray.length; i++) {
+        namesArray.push(studentsArray[i].name);
+    }
+    return namesArray.sort();
+}
+console.log(`Student names in alphabetical order:`, getStudentsNames());
+
+//Створіть функцію яка повертає кращого студента зі списку по показнику середньої оцінки.
+function getBestStudent(studentsArray = students) {
+    let marksArray = [];
+    for (let i = 0; i < studentsArray.length; i++) {
+        marksArray.push(getAverageMark(students[i]));
+    }
+    return studentsArray[marksArray.indexOf(Math.max(...marksArray))].name;
+}
+console.log(`Student with the highest average mark:`, getBestStudent());
+
+//Створіть функцію яка повертає обє'кт, в якому ключі це букви у слові, а значення – кількість їх повторень.
+function calculateWordLetters(string = "тест") {
+    let object = {};
+    for (let i = 0; i < string.length; i++) {
+        let letter = string[i];
+        if (object[letter] != undefined) {
+            ++object[letter];
+        } else {
+            object[letter] = 1;
+        };
+    }
+    return object;
+}
+console.log(`Function that counts a number of identical letters in a word:`, calculateWordLetters());
